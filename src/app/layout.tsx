4b7af35812f90355eabda232af26582920e74a93
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-08-07 12:41:53 +0200

    [feature] integrate locale management and message retrieval for internationalization support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { NextIntlClientProvider } from "next-intl";
+import { getLocale, getMessages } from "next-intl/server";
 import { Inter } from "next/font/google";
 export const dynamic = "auto";
 import "@/app/globals.css";
@@ -12,10 +14,15 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const locale = await getLocale();
+  const messages = await getMessages();
+
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang={locale} suppressHydrationWarning>
       <body className={inter.className}>
-        {children}
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
       </body>
     </html>
   );
